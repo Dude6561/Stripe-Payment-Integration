db98a5f903f97347bd4c30aa9aedcdcd7f0f1cd7
DUDE <nisbis194@gmail.com>
2025-12-22 09:47:55 +0545

    created a checkout page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
+  const amount = 49.99;
   return (
     <div>
-      <h1>this is payment integration</h1>
+      <div className=" justify-center items-center bg-white flex flex-col mx-50 h-screen">
+        <h1 className="text-black pb-6 font-bold text-xl">
+          Nischal Requested {amount}
+        </h1>
+        <button className="bg-white px-5 py-1 text-black border-1 hover:cursor-pointer hover:bg-gray-200 font-medium text-xl">
+          Pay
+        </button>
+      </div>
     </div>
   );
 }
